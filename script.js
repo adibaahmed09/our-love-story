ca603ec1f5e43f1d7a02bd231e0b9ba26d8543e0
adibaahmed09 <adibaahammad9@gmail.com>
2026-08-04 16:08:39 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
 function checkPassword() {
-
-    let password = document.getElementById("password").value;
-
-    if(password === "01012023"){
-
-        document.getElementById("message").innerHTML =
-        "💖 Correct Password!";
-
-    }
-
-    else{
-
-        document.getElementById("message").innerHTML =
-        "❌ Wrong Password";
-
-    }
-
+  alert("Button works!");
 }
