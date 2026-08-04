6f5bd63840a8ad84172af2fc15c4c57b6c18e00b
adibaahmed09 <adibaahammad9@gmail.com>
2026-08-04 16:21:34 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert("JavaScript Loaded!");
+function checkPassword() {
+
+    const password = document.getElementById("password").value;
+    const message = document.getElementById("message");
+
+    if (password === "01012023") {
+        message.style.color = "green";
+        message.textContent = "💖 Correct Password!";
+    } else {
+        message.style.color = "red";
+        message.textContent = "❌ Wrong Password!";
+    }
+
+}
